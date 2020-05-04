a4be2b548b1eba3f5d1fb8cafa8f801dc8f169bd
jguz17 <johnathanguzman17@gmail.com>
2020-05-04 17:17:25 -0500

    seed complete

---
+++ app/models/movie.rb
@@ -0,0 +1,4 @@
+class Movie < ApplicationRecord
+    has_many :order_items
+    has_many :orders, through: :order_items   
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# --- Users ---
+john = User.create(user_name: "John1234")
+yann = User.create(user_name: "Yann5678")
+bart = User.create(user_name: "Bart2468")
+
+# --- Movies ---
+hp = Movie.create(title: "Harry Potter")
+ae = Movie.create(title: "Avengers: End Game")
+lb = Movie.create(title: "Leprechaun 6: Back To Tha Hood")
+
+# --- Orders ---
+jo = Order.create(user: john)
+yo = Order.create(user: yann)
+bo = Order.create(user: bart)
+
+# --- Order_Item ---
+ 
+ji = OrderItem.create(order: jo, movie: hp)
+yi = OrderItem.create(order: yo, movie: lb)
+bi = OrderItem.create(order: bo, movie: ae)
